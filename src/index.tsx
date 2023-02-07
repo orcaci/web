f18224953f476ffb392a0cc3d8d2192af08f11a2
Ashwin <ashwin.sivanagarajan@gmail.com>
2023-02-07 20:44:05 +0530

    feat: :art: Initialized react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { router } from "./Routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </React.StrictMode>
 );
 
