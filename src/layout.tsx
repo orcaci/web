2d3d9be49e205e8600e263be7ad650185242adb0
Ashwin <ashwin.sivanagarajan@gmail.com>
2023-02-07 22:13:34 +0530

    feat: :sparkles: Added layout

---
+++ src/layout.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Layout } from "antd";
+import { TopNav } from "./components/topnav";
+import { LeftNav } from "./components/leftnav";
+import { ItemType } from "antd/es/menu/hooks/useItems";
+import { useNavigate } from "react-router-dom";
+
+const { Content } = Layout;
+
+const menuItems: ItemType[] = [
+  {
+    label: "Home",
+    key: "home"
+  },
+  {
+    label: "Section 1",
+    key: "section1"
+  }
+];
+
+interface Props {
+  children?: any;
+}
+
+export function AppLayout(props: Props) {
+  const navigate = useNavigate();
+  return (
+    <Layout>
+      <TopNav />
+      <Layout>
+        <LeftNav onClick={(e) => navigate(e.key)} menuItems={menuItems} />
+        <Layout>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: "calc(100vh - 64px)"
+            }}
+          >
+            {props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+}
+
