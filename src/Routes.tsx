0fa5611716fb68cd54f8b1b379d48717681680b7
Ashwin <ashwin.sivanagarajan@gmail.com>
2023-02-07 22:14:06 +0530

    feat: :construction: Sample route initialization

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Home } from "./pages/home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/home",
+    element: <Home />
   }
 ]);
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppLayout } from "../../layout";
+
+export function Home() {
+  return (
+    <AppLayout>
+      <div>HOME</div>
+    </AppLayout>
+  );
+}
