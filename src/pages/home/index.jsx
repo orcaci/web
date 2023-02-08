e4b4dab7804e671cb8e645d6f64295aefacce2c1
Ashwin <ashwin.sivanagarajan@gmail.com>
2023-02-08 19:37:46 +0530

    feat: :construction: Consumed Matrix in home component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { Matrix } from "../../components/matrix";
 
 export function Home() {
-  return <div>HOME</div>;
+  return (
+    <div>
+      <Matrix
+        defaultColumns={[
+          {
+            title: "Command",
+            dataIndex: "command",
+            editable: true
+          },
+          {
+            title: "Target",
+            dataIndex: "target",
+            editable: true
+          },
+          {
+            title: "Value",
+            dataIndex: "value",
+            editable: true
+          }
+        ]}
+      />
+    </div>
+  );
 }
