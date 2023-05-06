03a6904823b63958732246983a0a44adc4b1b2dc
Ashwin <ashwin.sivanagarajan@gmail.com>
2023-05-06 12:31:10 +0530

    feat: :sparkles: support for back navigation

--- src/components/pageHeader/index.tsx
+++ src/components/pageHeader/index.tsx
@@ -1,11 +1,16 @@
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 import { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface PageHeaderProps {
   title: string;
   extra?: ReactNode;
+  backIcon?: boolean;
 }
 
 export function PageHeader(props: PageHeaderProps) {
+  const navigate = useNavigate();
   return (
     <div
       className="page-header"
@@ -16,7 +21,18 @@ export function PageHeader(props: PageHeaderProps) {
         paddingBottom: "1rem"
       }}
     >
-      <p style={{ fontSize: 22, margin: 0 }}>{props.title}</p>
+      <p
+        style={{
+          fontSize: 22,
+          margin: 0,
+          display: "flex",
+          alignItems: "center",
+          gap:"0.5rem"
+        }}
+      >
+        {props.backIcon && <Button onClick={() => navigate(-1)} type="text" icon={<ArrowLeftOutlined />} />}
+        {props.title}
+      </p>
       <div className="extra">{props.extra}</div>
     </div>
   );
