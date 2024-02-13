c6f008f102f882a0f265c565823c62ee4bf2c794
Vasanth kumar Kalaiselvan <itsparser@gmail.com>
2024-02-13 10:57:36 +0530

    Added sample code for react flow

---
+++ src/stores/route.store.ts
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+
+export const orcaStore = create((set) => ({
+  appActiveMenu: "dashboard",
+  setAppActiveMenu: (menu: string) => set({ appActiveMenu: menu })
+}));
